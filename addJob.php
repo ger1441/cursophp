2c4b0a263ff0745ca290dba463b241553cdc2d19
= <=>
2020-03-11 02:29:36 +0000

    Curso PHP Basico Limpiando las carpetas para comenzar

---
+++ addJob.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Add Job</title>
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/css/bootstrap.min.css" integrity="sha384-Smlep5jCw/wG7hdkwQ/Z5nLIefveQRIY9nfy6xoR1uRYBtpZgI6339F5dgvm/e9B"
+          crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1>Add Job</h1>
+    <form action="">
+        <label for="">Title:</label>
+        <input type="text"><br>
+        <label for="">Description:</label>
+        <input type="text">
+    </form>
+</body>
+</html>
+
