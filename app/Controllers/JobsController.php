2822a553509f7aa9f32cb479288a27869edd6b87
= <=>
2020-03-18 19:49:39 +0000

    Curso PHP Básico Validaciones [Validator]

--- app/Controllers/JobsController.php
+++ app/Controllers/JobsController.php
@@ -1,19 +1,37 @@
 <?php
 namespace App\Controllers;
 use App\Models\Job;
+use Respect\Validation\Validator;
 
 class JobsController extends BaseController
 {
     public function getAddJobAction($request){
+        $responseMessage = "";
+        $classMessage = "";
         if($request->getMethod() == "POST"){
             $postData = $request->getParsedBody();
 
-            $job = new Job;
-            $job->title = $postData['title'];
-            $job->description = $postData['description'];
-            $job->save();
+            $jobValidator = Validator::key('title', Validator::stringType()->notEmpty())
+                ->key('description', Validator::stringType()->notEmpty());
+
+            try{
+                $jobValidator->assert($postData);
+                $job = new Job;
+                $job->title = $postData['title'];
+                $job->description = $postData['description'];
+                $job->save();
+                $responseMessage = 'Save Job success!';
+                $classMessage = 'success';
+            }catch(\Exception $e) {
+                //var_dump($e->getMessage());
+                $responseMessage = $e->getMessage();
+                $classMessage = 'info';
+            }
         }
 
-        return $this->renderHTML('addJob.twig');
+        return $this->renderHTML('addJob.twig',[
+            'responseMessage'=>$responseMessage,
+            'classMessage'=>$classMessage
+        ]);
     }
 }
--- app/Controllers/ProjectsController.php
+++ app/Controllers/ProjectsController.php
@@ -2,17 +2,33 @@
 namespace App\Controllers;
 
 use App\Models\Project;
+use Respect\Validation\Validator;
 
 class ProjectsController extends BaseController
 {
     public function getAddProjectAction($request){
+        $responseMessage = "";
+        $classMessage = "";
         if($request->getMethod()=="POST"){
             $postData = $request->getParsedBody();
-            $project = new Project;
-            $project->title = $postData['title'];
-            $project->description = $postData['description'];
-            $project->save();
+            $projectValidator = Validator::key('title',Validator::stringType()->notEmpty())
+                ->key('description',Validator::stringType()->notEmpty());
+            try {
+                $projectValidator->assert($postData);
+                $project = new Project;
+                $project->title = $postData['title'];
+                $project->description = $postData['description'];
+                $project->save();
+                $responseMessage = "Job Save success";
+                $classMessage = "success";
+            }catch(\Exception $e){
+                $responseMessage = $e->getMessage();
+                $classMessage = "info";
+            }
         }
-        return $this->renderHTML('addProject.twig');
+        return $this->renderHTML('addProject.twig',[
+            'responseMessage' => $responseMessage,
+            'classMessage' => $classMessage,
+        ]);
     }
 }
