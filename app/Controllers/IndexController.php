9bdb12c164fd9e2062bfd4cb83266b3fe8d191ce
= <=>
2020-03-29 05:39:42 +0000

    Curso PHP Avanzado | Closure

--- app/Controllers/IndexController.php
+++ app/Controllers/IndexController.php
@@ -8,11 +8,17 @@ class IndexController extends BaseController
 {
     public function indexAction(){
 
+        $name = "Yerman QL";
+        $limitMonths = 15;
+
         $jobs = Job::all();
         $projects = Project::all();
 
-        $name = "Yerman QL";
-        $limitMonths = 2000;
+        $filterFunction = function($job) use ($limitMonths){
+            return $job['months'] >= $limitMonths;
+        };
+
+        $jobs = array_filter($jobs->toArray(),$filterFunction);
 
         return $this->renderHTML('index.twig',[
             'name' => $name,
