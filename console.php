0c7c830f25a7c6fb7c453b17d8f6968754000b8a
= <=>
2020-04-05 01:40:17 +0000

    Curso PHP Avanzado | Comandos

--- console.php
+++ console.php
@@ -1,33 +1,13 @@
 #!/usr/bin/env php
 <?php
+#!/usr/bin/env php <--Esta linea indica a la terminal hacia donde debe de ir la ejecución
 
 require __DIR__.'/vendor/autoload.php';
 
 use Symfony\Component\Console\Application;
-use Illuminate\Database\Capsule\Manager as Capsule;
-
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-$dotenv->load();
-
-$capsule = new Capsule;
-$capsule->addConnection([
-    'driver'    => 'mysql',
-    'host'      => getenv('DB_HOST'),
-    'database'  => getenv('DB_NAME'),
-    'username'  => getenv('DB_USER'),
-    'password'  => getenv('DB_PASS'),
-    'charset'   => 'utf8',
-    'collation' => 'utf8_unicode_ci',
-    'prefix'    => '',
-]);
-// Make this Capsule instance available globally via static methods... (optional)
-$capsule->setAsGlobal();
-// Setup the Eloquent ORM... (optional; unless you've used setEventDispatcher())
-$capsule->bootEloquent();
 
 $application = new Application();
-// ... register commands
+
 $application->add(new \App\Commands\HelloWorldCommand());
-$application->add(new \App\Commands\SendMailCommand());
-$application->add(new \App\Commands\CreateUserCommand());
+
 $application->run();
